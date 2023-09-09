0e1c074f79ee5322ea7a874096695ef3b5d546f8
Omkar Chandorkar <gotenksIN@aospa.co>
2023-09-09 15:55:03 +0900

    Introduce Dolby Atmos support
    Change-Id: I46b62d08ad51c321c997792301524fc0c18d6ba6
    Co-authored-by: Sugakesshaa <sugakesshaa@gmail.com>
    Signed-off-by: Omkar Chandorkar <gotenksIN@aospa.co>
    Signed-off-by: basamaryan <basam.aryan@gmail.com>

--- device.mk
+++ device.mk
@@ -6,6 +6,9 @@
 # Inherit from the proprietary version
 $(call inherit-product, vendor/oneplus/dolby/dolby-vendor.mk)
 
+# HIDL
+DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE += $(LOCAL_PATH)/configs/hidl/dolby_framework_matrix.xml
+
 # Soong namespaces
 PRODUCT_SOONG_NAMESPACES += \
     $(LOCAL_PATH)
