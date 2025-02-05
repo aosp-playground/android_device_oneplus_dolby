62319134cf5ce4bb84ac4703d17bc3322af909bf
Sugakesshaa <sugakesshaa@pixelos.net>
2025-02-05 19:12:48 +0800

    Import Dolby C2 Media service
    from OnePlus 11 CPH2447_15.0.0.401(EX01)
    
    Change-Id: Icdfc5b11c7635d0707c2a39f54e0c8d149c771bc

--- device.mk
+++ device.mk
@@ -5,3 +5,10 @@
 
 # Inherit from the proprietary version
 $(call inherit-product, vendor/oneplus/dolby/dolby-vendor.mk)
+
+# HIDL
+ODM_MANIFEST_FILES += $(LOCAL_PATH)/configs/hidl/c2_manifest.xml
+
+# Media
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/media/media_codecs_c2_dolby_audio.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs_c2_dolby_audio.xml
